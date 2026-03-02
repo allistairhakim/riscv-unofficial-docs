0ac320a7f886052ffcb7eaa7e8065811f55679a5
allistairhakim <allistair.hakim@gmail.com>
2026-03-02 09:52:17 -0500

    Fixed links and added favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://allistairhakim.github.io',
-  base: '/riscv-unofficial-docs',
+  base: '/riscv-unofficial-docs'
 });
